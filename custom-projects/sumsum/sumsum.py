17bff73061fc6e392006548c09926947a5a2d0df
Sami <s.kostilainen@gmail.com>
2023-04-11 17:17:37 +0300

    feat(sum): remove mp3 files at the end and add transcript api

--- custom-projects/sumsum/sumsum.py
+++ custom-projects/sumsum/sumsum.py
@@ -2,8 +2,11 @@ import openai
 import base64
 import yt_dlp
 import time
-from pydub import AudioSegment
 import streamlit as st
+import os
+import glob
+from pydub import AudioSegment
+from youtube_transcript_api import YouTubeTranscriptApi
 
 # define openai api key and stuff
 openai.api_key = (st.secrets["OPENAPI_TOKEN"])
@@ -84,10 +87,33 @@ def get_transcript(output_file):
 
     return transcript.text
 
+def concat_transcripts(transcripts):
+    concatted_transcript = ""
+
+    for line in transcripts:
+        concatted_transcript += line['text'] + " "
+
+    return concatted_transcript.strip()
+
+def get_concatted_transcript(url):
+    video_id = url.split('watch?v=')[-1]
+    transcripts = get_transcript_by_id(video_id)
+    concatted_transcripts = concat_transcripts(transcripts)
+
+    return concatted_transcripts
+
 def do_the_thing(url):
-    output_file = download_audio_from_video(url)
-    transcript = get_transcript(output_file)
+    transcript = get_concatted_transcript(url)
+
+    if transcript is None:
+        print("No transcript found, downloading audio from video")
+        output_file = download_audio_from_video(url)
+        transcript = get_transcript(output_file)
+    else:
+        print("Found transcript")
+
     summary = summarize_text(transcript)
+    delete_mp3_files()
 
     return summary
 
@@ -96,3 +122,23 @@ def get_title(url):
     print(video_info)
 
     return video_info['title']
+
+def get_transcript_by_id(video_id, language_code='en'):
+    try:
+        transcript = YouTubeTranscriptApi.get_transcript(video_id, languages=[language_code])
+        return transcript
+    except Exception as e:
+        print(f"Error getting transcript: {e}")
+        return None
+    
+
+def delete_mp3_files():
+    current_directory = os.path.dirname(os.path.abspath(__file__))
+    mp3_files = glob.glob(os.path.join(current_directory, '*.mp3'))
+
+    for mp3_file in mp3_files:
+        try:
+            os.remove(mp3_file)
+            print(f"Deleted {mp3_file}")
+        except Exception as e:
+            print(f"Error deleting {mp3_file}: {e}")
