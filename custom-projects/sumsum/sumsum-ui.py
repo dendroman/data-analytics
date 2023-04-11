a551cb2f1634679df89e3960094ffa894a82d6eb
Sami <s.kostilainen@gmail.com>
2023-04-11 17:30:07 +0300

    feat(ui): adjusted the layout and titles

--- custom-projects/sumsum/sumsum-ui.py
+++ custom-projects/sumsum/sumsum-ui.py
@@ -1,10 +1,7 @@
 import streamlit as st
 from sumsum import do_the_thing, get_title
 
-st.title("Hello, World!")
-
-st.write("This is SumSum, a audio/video summarizer!")
-
+st.markdown("## SumSum - A YouTube Summarizer :movie_camera:")
 st.write("Enter a YouTube URL to get started!")
 
 url = st.text_input("URL")
@@ -13,4 +10,4 @@ if st.button("Summarize"):
     st.write("Summarizing " + get_title(url) + "")
     st.write(do_the_thing(url))
 
-st.write("Made by @dendroman")
+st.markdown("Made by [@dendroman](https://github.com/dendroman/)")
